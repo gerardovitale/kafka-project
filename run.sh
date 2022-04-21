ff19eb4019d140e93f71aa8d1c28f2b5ec480c0f
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2022-04-21 16:42:59 +0200

    check added to verify if topic was successfully created

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ source ./.env
 
 startDockerEnv() {
   if [[ $1 == prune ]]; then
-    docker-compose stop
+    docker-compose stop || exit 1
     docker container prune
   fi
   docker-compose up -d || exit 1
@@ -20,27 +20,34 @@ createKafkaTopics() {
   elif [[ $restProxyHealthCheck == 200 ]]; then
     cluster_id=$(curl -s "$KAFKA_REST_PROXY_SERVER"/v3/clusters/ | grep -oE '"cluster_id":"(.*?)"' |
       sed -e 's/^"cluster_id":"//' -e 's/"$//')
-    echo "[INFO] getting kafka cluster id: $cluster_id"
+    echo "[INFO] got kafka cluster id: $cluster_id"
 
     topic_res=$(curl -sw "%{http_code}" "$KAFKA_REST_PROXY_SERVER"/topics/"$KAFKA_TOPIC" | tail -c 3)
 
     if [[ $topic_res != 200 ]]; then
-      echo "[INFO] creating kafka topic: $KAFKA_TOPIC"
-      curl --header "Content-Type: application/json" \
+      topicCreated=$(curl -sw "%{http_code}" \
+        --header "Content-Type: application/json" \
         --request POST \
         --data "{\"topic_name\":\"$KAFKA_TOPIC\",\"partitions_count\":1,\"replication_factor\": 1,\"configs\":[]}" \
-        -s "$KAFKA_REST_PROXY_SERVER"/v3/clusters/"$cluster_id"/topics
+        "$KAFKA_REST_PROXY_SERVER"/v3/clusters/"$cluster_id"/topics |
+        tail -c 3)
+
+        if [[ $topicCreated == 201 ]]; then
+          echo "[INFO] topic $KAFKA_TOPIC created"
+        else
+          echo "[ERROR] topic $KAFKA_TOPIC wasn't created"
+        fi
 
     elif [[ $topic_res == 200 ]]; then
-      echo "[INFO] topic already exists: $KAFKA_TOPIC"
+      echo "[WARN] topic already exists: $KAFKA_TOPIC"
 
     else
-      echo "topic error code $topic_res"
+      echo "[ERROR] topic error code $topic_res"
       exit 1
     fi
 
   else
-    echo "restProxyHealthCheck error code $restProxyHealthCheck"
+    echo "[ERROR] restProxyHealthCheck error code $restProxyHealthCheck"
     exit 1
   fi
 
