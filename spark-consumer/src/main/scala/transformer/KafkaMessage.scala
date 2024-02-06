70e73ab3d0bc849818797fbda6ec074c0df99436
Gerardo Vitale <gerardo.vitale@thoughtworks.com>
2024-02-06 15:37:48 +0100

    add getMessageRatePerMinutes

--- spark-consumer/src/main/scala/transformer/KafkaMessage.scala
+++ spark-consumer/src/main/scala/transformer/KafkaMessage.scala
@@ -1,12 +1,18 @@
 package transformer
 
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, from_json}
+import org.apache.spark.sql.functions.{col, from_json, to_timestamp, window}
 import org.apache.spark.sql.types.StructType
 
 object KafkaMessage {
 
-  def denormalize(df: DataFrame, schema: StructType): DataFrame =
-    df.select(from_json(col("value").cast("string"), schema).as("data"))
+  def denormalize(dataFrame: DataFrame, schema: StructType): DataFrame =
+    dataFrame.select(from_json(col("value").cast("string"), schema).as("data"))
       .select("data.*")
+
+  def getMessageRatePerMinutes(dataFrame: DataFrame): DataFrame =
+    dataFrame.withColumn("timestamp", to_timestamp(col("timestamp")))
+      .withWatermark("timestamp", "30 Minutes")
+      .groupBy(window(col("timestamp"), "1 Minutes").alias("time_window"))
+      .count()
 }
--- spark-consumer/src/test/scala/transformer/KafkaMessageSpec.scala
+++ spark-consumer/src/test/scala/transformer/KafkaMessageSpec.scala
@@ -2,7 +2,7 @@ package transformer
 
 import configTest.SparkSessionTestWrapper
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.types._
 import org.scalatest.funsuite.AnyFunSuite
 import schemaReader.Schema
 
@@ -20,10 +20,9 @@ class KafkaMessageSpec extends AnyFunSuite with SparkSessionTestWrapper {
     )
     val testDF = spark.createDataFrame(spark.sparkContext.parallelize(testData), testSchema)
 
-    val expectedSchema = StructType(Seq(
-      StructField("name", StringType, nullable = true),
-      StructField("type", StringType, nullable = true),
-    ))
+    val expectedSchema = new StructType()
+      .add("name", StringType, nullable = true)
+      .add("type", StringType, nullable = true)
     val expectedData = Seq(
       Row("test_name1", "test_type1"),
       Row("test_name2", "test_type2"),
@@ -32,6 +31,42 @@ class KafkaMessageSpec extends AnyFunSuite with SparkSessionTestWrapper {
     val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), expectedSchema)
 
     val actualDF = KafkaMessage.denormalize(testDF, expectedSchema)
+
+    assert(expectedDF.schema == actualDF.schema)
+    assert(expectedDF.exceptAll(actualDF).isEmpty)
+  }
+
+  test("aggregate kafka message") {
+    val testSchema = new StructType()
+      .add("id", IntegerType, nullable = true)
+      .add("type", StringType, nullable = true)
+      .add("timestamp", IntegerType, nullable = true)
+    val testData = Seq(
+      // Row("ID", "type" "timestamp"),
+      Row(1234567890, "edit", 1707227685),
+      Row(1234567891, "edit", 1707227685),
+      Row(1234567892, "edit", 1707227685 + 60),
+      Row(1234567892, "edit", 1707227685 + 61),
+      Row(1234567892, "edit", 1707227685 + 62),
+      Row(1234567892, "edit", 1707227685 + 120),
+    )
+    val testDF = spark.createDataFrame(spark.sparkContext.parallelize(testData), testSchema)
+
+    val expectedTimeWindowSchema = new StructType()
+      .add("start", TimestampType, nullable = true)
+      .add("end", TimestampType, nullable = true)
+    val expectedSchema = new StructType()
+      .add("time_window", expectedTimeWindowSchema, nullable = false)
+      .add("count", LongType, nullable = false)
+    val expectedData = Seq(
+      // Row("time_window", "count"),
+      Row(Row(Timestamp.valueOf("2024-02-06 14:54:00"), Timestamp.valueOf("2024-02-06 14:55:00")), 2L),
+      Row(Row(Timestamp.valueOf("2024-02-06 14:55:00"), Timestamp.valueOf("2024-02-06 14:56:00")), 3L),
+      Row(Row(Timestamp.valueOf("2024-02-06 14:56:00"), Timestamp.valueOf("2024-02-06 14:57:00")), 1L),
+    )
+    val expectedDF = spark.createDataFrame(spark.sparkContext.parallelize(expectedData), expectedSchema)
+
+    val actualDF = KafkaMessage.getMessageRatePerMinutes(testDF)
 
     assert(expectedDF.schema == actualDF.schema)
     assert(expectedDF.exceptAll(actualDF).isEmpty)
